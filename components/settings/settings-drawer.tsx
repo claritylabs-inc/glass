800c0057f474b8c536eb0383227e723202be2e0b
Terry Wang <wang.terry375@gmail.com>
2026-06-05 00:17:37 -0700

    Limit full-width drawer footer buttons to mobile

--- components/settings/settings-drawer.tsx
+++ components/settings/settings-drawer.tsx
@@ -58,7 +58,7 @@ export function SettingsDrawer({
             </div>
 
             {footer && (
-              <div className="border-t border-foreground/6 px-4 py-3 flex flex-col-reverse items-stretch gap-2 shrink-0 lg:flex-row lg:items-center lg:justify-end [&>button]:w-full [&>button]:min-h-8 [&>button]:py-2 lg:[&>button]:w-auto lg:[&>button]:min-h-7 lg:[&>button]:py-1">
+              <div className="border-t border-foreground/6 px-4 py-3 flex flex-col-reverse items-stretch gap-2 shrink-0 sm:flex-row sm:items-center sm:justify-end [&>button]:w-full [&>button]:min-h-8 [&>button]:py-2 sm:[&>button]:w-auto sm:[&>button]:min-h-7 sm:[&>button]:py-1">
                 {footer}
               </div>
             )}
