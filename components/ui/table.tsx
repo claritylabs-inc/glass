3577c0002f55d4768e90b4a7f0b8bf3f7764f35c
Terry Wang <wang.terry375@gmail.com>
2026-08-24 15:32:48 +0000

    Ensure tables fill their containers

--- components/ui/table.tsx
+++ components/ui/table.tsx
@@ -13,7 +13,7 @@ function Table({ className, ...props }: React.ComponentProps<"table">) {
     >
       <table
         data-slot="table"
-        className={cn(`w-max min-w-full caption-bottom ${typeStyle("body.default")}`, className)}
+        className={cn(`w-full min-w-full caption-bottom ${typeStyle("body.default")}`, className)}
         {...props}
       />
     </div>
